6de6894f426123a8e86049215bcc9f4137e66718
vpeil <vitali.peil@uni-bielefeld.de>
2014-06-18 14:59:12 +0200

    pod, bug fix, milla

--- lib/Catmandu/CrossRef.pm
+++ lib/Catmandu/CrossRef.pm
@@ -1,6 +1,22 @@
 package Catmandu::CrossRef;
-# ABSTRACT: makes something
+
 
 our $VERSION = '0.001';
 
 1;
+
+=head1 NAME
+
+	Catmandu::CrossRef - a bundle of Catmandu modules for working with data from CrossRef
+
+=head1 MODULES
+
+=over
+
+=item *
+
+L<Catmandu::Importer::CrossRef>
+
+=back
+
+=cut
